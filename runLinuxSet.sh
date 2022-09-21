56839c7d24bec3368b0b708eacea9e67902b84c1
Jave <gifanshawe@gmail.com>
2022-09-21 19:51:06 -0400

    Update runLinuxSet.sh

--- runLinuxSet.sh
+++ runLinuxSet.sh
@@ -6,7 +6,7 @@
 TARGET_DIR="MySetup/"
 LOG_FILE="log.txt"
 PACKAGES="vim vim-gtk3 emacs tmux youtube-dl ffmpeg python3 python python-pip gcc g++ valgrind clisp ghc gdb 
-synaptic gedit default-jdk mpv xdotool redshift-gtk"
+synaptic gedit default-jdk mpv xdotool redshift-gtk pandoc"
 
 
 PACKAGE_TEST="xdotool"
